344333c55f635ce9284f9c4526686f674311b593
Rivan012 <114328078+Rivan012@users.noreply.github.com>
2025-09-09 16:32:55 +0700

    Create Main.java

---
+++ Enkapsulasi/Main.java
@@ -0,0 +1,21 @@
+class Mahasiswa{
+    private String nama;
+    private int umur;
+
+    public void setDataMahasiswa(String nama, int umur){
+        this.nama = nama;
+        this.umur = umur;
+    }
+    public String getDataMahasiswa(){
+        return "Nama Saya "+ this.nama + " dan umur saya adalah " + this.umur ;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.setDataMahasiswa("Rivan Alfatoni", 19);
+        System.out.println(mhs1.getDataMahasiswa());
+        
+    }
+}
